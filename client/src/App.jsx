1e4793afb2c77b22ce8f5648f8c7708bb22ae5ac
debraj <debtajnath@gmail.com>
2023-12-25 13:22:35 +0530

    massage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import {BrowserRouter, Route, Routes} from"react-router-dom"
 import Home from './pages/Home'
-
+import About from './pages/About'
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +13,11 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route  path='/' element = {<Home/>}/>
+        <Route  path='/About' element = {<About/>}/>
+        <Route  path='/SignIn' element = {<SignIn/>}/>
+        <Route  path='/SignUp' element = {<SignUp/>}/>
+        <Route  path='/Profile' element = {<Profile/>}/>
+
       </Routes>
       </BrowserRouter>
    
