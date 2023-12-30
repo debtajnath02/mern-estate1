92f02c754ea662d318757748af6365684487a6c1
debraj <debtajnath@gmail.com>
2023-12-30 13:24:18 +0530

    including search option in navbar

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import{FaSearch} from "react-icons/fa"
 const Header = () => {
   return (
     <div className="bg-slate-300">
       <div className="flex justify-between items-center max-w-8xl mx-auto p-3">
+
+
+
         <Link to="/">
           <h1 className="font-bold">mern auth</h1>
         </Link>
 
+<form action="" className="bg-slate-100 p-3 rounded-lg flex item-center">
+  <input type="text" className="bg-transparent focus:outline-none"/>
+  <FaSearch className="text-slate-600"/>
+</form>
         <ul className="flex gap-4">
           <Link to="/">
             <li>Home</li>
